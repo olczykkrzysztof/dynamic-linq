acaf92eb57f25ce05fd1fddd59b0d551ba879683
Krzysztof Olczyk <olczyk.krzysztof+git@gmail.com>
2012-01-25 21:24:19 +0100

    DynamicLinq: Added possibility to construct the existing type in "new" statement.

--- DynamicLinq/DynamicQuery/Dynamic.cs
+++ DynamicLinq/DynamicQuery/Dynamic.cs
@@ -1,4 +1,30 @@
-﻿//Copyright (C) Microsoft Corporation.  All rights reserved.
+﻿/*Microsoft Public License (MS-PL)
+
+This license governs use of the accompanying software. If you use the software, you
+ accept this license. If you do not accept the license, do not use the software.
+
+1. Definitions
+ The terms "reproduce," "reproduction," "derivative works," and "distribution" have the
+ same meaning here as under U.S. copyright law.
+ A "contribution" is the original software, or any additions or changes to the software.
+ A "contributor" is any person that distributes its contribution under this license.
+ "Licensed patents" are a contributor's patent claims that read directly on its contribution.
+
+2. Grant of Rights
+ (A) Copyright Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, each contributor grants you a non-exclusive, worldwide, royalty-free copyright license to reproduce its contribution, prepare derivative works of its contribution, and distribute its contribution or any derivative works that you create.
+ (B) Patent Grant- Subject to the terms of this license, including the license conditions and limitations in section 3, each contributor grants you a non-exclusive, worldwide, royalty-free license under its licensed patents to make, have made, use, sell, offer for sale, import, and/or otherwise dispose of its contribution in the software or derivative works of the contribution in the software.
+
+3. Conditions and Limitations
+ (A) No Trademark License- This license does not grant you rights to use any contributors' name, logo, or trademarks.
+ (B) If you bring a patent claim against any contributor over patents that you claim are infringed by the software, your patent license from such contributor to the software ends automatically.
+ (C) If you distribute any portion of the software, you must retain all copyright, patent, trademark, and attribution notices that are present in the software.
+ (D) If you distribute any portion of the software in source code form, you may do so only under this license by including a complete copy of this license with your distribution. If you distribute any portion of the software in compiled or object code form, you may only do so under a license that complies with this license.
+ (E) The software is licensed "as-is." You bear the risk of using it. The contributors give no express warranties, guarantees or conditions. You may have additional consumer rights under your local laws which this license cannot change. To the extent permitted under your local laws, the contributors exclude the implied warranties of merchantability, fitness for a particular purpose and non-infringement.
+*/
+
+//Copyright (C) Microsoft Corporation.  All rights reserved.
+
+// Specification of classes in new clause - Copyright (c) 2012 Krzysztof Olczyk.
 
 using System;
 using System.Collections.Generic;
@@ -1025,6 +1051,31 @@ namespace System.Linq.Dynamic
 
         Expression ParseNew() {
             NextToken();
+			
+			bool anonymous = true;
+			Type class_type = null;
+			
+			if (token.id == TokenId.Identifier)
+			{
+				anonymous = false;
+				StringBuilder full_type_name = new StringBuilder(GetIdentifier());
+				
+				NextToken();
+				
+				while (token.id == TokenId.Dot)
+				{
+					NextToken();
+					ValidateToken(TokenId.Identifier, Res.IdentifierExpected);
+					full_type_name.Append(".");
+					full_type_name.Append(GetIdentifier());
+					NextToken();
+				}
+				
+				class_type = Type.GetType(full_type_name.ToString(), false);	
+				if (class_type == null)
+					throw ParseError(Res.TypeNotFound, full_type_name.ToString());
+			}
+				
             ValidateToken(TokenId.OpenParen, Res.OpenParenExpected);
             NextToken();
             List<DynamicProperty> properties = new List<DynamicProperty>();
@@ -1050,7 +1101,8 @@ namespace System.Linq.Dynamic
             }
             ValidateToken(TokenId.CloseParen, Res.CloseParenOrCommaExpected);
             NextToken();
-            Type type = DynamicExpression.CreateClass(properties);
+            //Type type = DynamicExpression.CreateClass(properties);
+			Type type = anonymous ? DynamicExpression.CreateClass(properties) : class_type; 
             MemberBinding[] bindings = new MemberBinding[properties.Count];
             for (int i = 0; i < bindings.Length; i++)
                 bindings[i] = Expression.Bind(type.GetProperty(properties[i].Name), expressions[i]);
@@ -2017,6 +2069,7 @@ namespace System.Linq.Dynamic
         public const string ParseExceptionFormat = "{0} (at index {1})";
         public const string ColonExpected = "':' expected";
         public const string OpenParenExpected = "'(' expected";
+		public const string TypeNotFound = "Type {0} not found";
         public const string CloseParenOrOperatorExpected = "')' or operator expected";
         public const string CloseParenOrCommaExpected = "')' or ',' expected";
         public const string DotOrOpenParenExpected = "'.' or '(' expected";
